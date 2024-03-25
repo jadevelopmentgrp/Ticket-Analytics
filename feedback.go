1b94418d0c65aebca1e26f3c437b2a18909eacf8
rxdn <29165304+rxdn@users.noreply.github.com>
2024-03-25 21:02:41 +0000

    Add new aggregation query functions

---
+++ feedback.go
@@ -0,0 +1,32 @@
+package analytics
+
+import (
+	"context"
+	_ "embed"
+)
+
+var (
+	//go:embed sql/average_rating_guild.sql
+	queryGetAverageFeedbackRatingGuild string
+
+	//go:embed sql/feedback_count_guild.sql
+	queryGetFeedbackCountGuild string
+)
+
+func (c *Client) GetAverageFeedbackRatingGuild(ctx context.Context, guildId uint64) (float32, error) {
+	var rating float32
+	if err := c.client.QueryRow(ctx, queryGetAverageFeedbackRatingGuild, guildId).Scan(&rating); err != nil {
+		return 0, err
+	}
+
+	return rating, nil
+}
+
+func (c *Client) GetFeedbackCountGuild(ctx context.Context, guildId uint64) (int, error) {
+	var count int
+	if err := c.client.QueryRow(ctx, queryGetFeedbackCountGuild, guildId).Scan(&count); err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
---
+++ sql/total_ticket_count.sql
@@ -0,0 +1,3 @@
+SELECT countMerge(count) AS total_count
+FROM analytics.total_ticket_count
+WHERE guild_id = ?;
